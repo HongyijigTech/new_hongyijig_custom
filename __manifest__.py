3a21ae79bbf9b366809eb2d7a9833760992b32d2
Ridhi Khattar <ridhikhattar@Ridhis-MacBook-Pro-2.local>
2026-08-28 20:42:32 +0530

    Correct programme milestone classifications

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'HongyiJigTechNew',
-    'version': '1.21',
+    'version': '1.22',
     'summary': 'Hongyijig Tech New',
     'sequence': 10,
     'description': """
