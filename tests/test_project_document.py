6e2889071fb00eae907b97075977f3ae7c5f8bb6
Ridhi Khattar <ridhikhattar@Ridhis-MacBook-Pro-2.local>
2026-08-29 11:09:23 +0530

    Plan project staffing without Odoo seats

--- tests/test_project_document.py
+++ tests/test_project_document.py
@@ -108,7 +108,7 @@ class TestProjectDocumentGovernance(TransactionCase):
     def test_project_code_locks_after_first_document(self):
         self._create_document()
         with self.assertRaises(ValidationError):
-            self.project.x_project_code = "HJ-LGC-2026-0002"
+            self.project.x_project_code = "HJ-TST-2099-9002"
 
     def test_master_reference_cannot_enter_customer_register(self):
         with self.assertRaises(ValidationError):
@@ -161,6 +161,39 @@ class TestProjectDocumentGovernance(TransactionCase):
         document = self._create_document()
         with self.assertRaises(UserError):
             document.action_submit_review()
+
+    def test_global_holder_has_no_authority_without_project_assignment(self):
+        other_project = self.env["project.project"].create({
+            "name": "Other Controlled Project",
+            "hjig_project_record_type": "customer",
+            "x_project_code": "HJ-TST-2099-9010",
+        })
+        document = self._create_document(project_id=other_project.id)
+        with self.assertRaises(UserError):
+            document.with_user(self.owner).action_submit_review()
+
+    def test_planned_resource_does_not_grant_login_authority(self):
+        project = self.env["project.project"].create({
+            "name": "Pre-Seat Planning Project",
+            "hjig_project_record_type": "customer",
+            "x_project_code": "HJ-TST-2099-9011",
+        })
+        resource = self.env["hjig.planned.team.resource"].create({
+            "name": "Future Tool Engineer",
+            "resource_type": "internal",
+            "planned_capacity": 2,
+        })
+        assignment = self.env["hjig.project.designation.assignment"].create({
+            "project_id": project.id,
+            "designation_id": self.owner_designation.id,
+            "planned_resource_ids": [(6, 0, [resource.id])],
+        })
+        self.assertEqual(assignment.staffing_status, "planned")
+        self.assertFalse(assignment.holder_ids)
+        self.assertFalse(self.owner_designation._holders_for_project(project))
+        document = self._create_document(project_id=project.id)
+        with self.assertRaises(UserError):
+            document.with_user(self.owner).action_submit_review()
 
     def test_status_cannot_bypass_workflow(self):
         document = self._create_document()
